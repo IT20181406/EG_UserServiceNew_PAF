7f2a9036febda2491dd2575857094eb3f8dd67ef
IT20181406 <it20181406@my.sllit.lk>
2022-05-15 22:12:09 +0530

    Creat a new repo beacuse failure before one

---
+++ EG_UserService/src/com/UserManagment.java
@@ -0,0 +1,186 @@
+package com;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.Random;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+
+public class UserManagment {
+
+	private Connection connect() {
+		Connection con = null;
+		try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+
+			// Provide the correct details: DBServer/DBName, username, password
+			con = DriverManager.getConnection(
+					"jdbc:mysql://localhost:3306/jerseyproject?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC",
+					"root", "Admin123");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return con;
+	}
+
+	public String insertProject(String name, String email, String password,String AccountNo, String status) {
+		String output = "";
+		try {
+			Random random = new Random();
+			int otp = random.nextInt(123456);
+			Connection con = connect();
+			if (con == null) {
+				return "Error while connecting to the database for inserting.";
+			}
+			// create a prepared statement
+			String query = " insert into user(`id`, `name`, `email`, `password`, `AccountNo`, `otp`, `status`)"
+					+ " values ( ?, ?, ?, ?, ?, ?, ?)";
+			PreparedStatement preparedStmt = con.prepareStatement(query);
+			// binding values
+			preparedStmt.setInt(1, 0);
+			preparedStmt.setString(2, name);
+			preparedStmt.setString(3, email);
+			preparedStmt.setString(4, password);
+			preparedStmt.setString(5, AccountNo);
+			preparedStmt.setInt(6, otp);
+			preparedStmt.setString(7, "In Active");
+
+			// execute the statement
+			preparedStmt.execute();
+			con.close();
+
+			String newProject = readProject();
+			output = "{\"status\":\"success\", \"data\": \"" + newProject + "\"}";
+		} catch (Exception e) {
+			output = "{\"status\":\"error\", \"data\": \"Error while inserting the project.\"}";
+			System.err.println(e.getMessage());
+		}
+
+		return output;
+	}
+
+	public String readProject() {
+		String output = "";
+		try {
+			Connection con = connect();
+			if (con == null) {
+				return "Error while connecting to the database for reading.";
+			}
+			// Prepare the html table to be displayed
+			output = "<table border=\'1\'><tr><th> Name </th><th> Email </th><th> Password </th><th> Account No</th><th> OTP </th><th> Status </th><th> Update </th><th> Delete </th></tr>";
+			String query = "select * from user";
+
+			Statement stmt = (Statement) con.createStatement();
+			ResultSet rs = ((java.sql.Statement) stmt).executeQuery(query);
+			// iterate through the rows in the result set
+			while (rs.next()) {
+				String id = Integer.toString(rs.getInt("id"));
+				String name = rs.getString("name");
+				String email = rs.getString("email");
+				String password = rs.getString("password");
+				String AccountNo = rs.getString("AccountNo");
+				String otp = rs.getString("otp");
+				String status = rs.getString("status");
+
+				// Add into the html table
+				output += "<tr><td><input id=\'hidProjectIDUpdate\' name=\'hidProjectIDUpdate\' type=\'hidden\' value=\'"
+						+ id + "'>" + name + "</td>";
+				output += "<td>" + email + "</td>";
+				output += "<td>" + password + "</td>";
+				output += "<td>" + AccountNo + "</td>";
+				output += "<td>" + otp + "</td>";
+				output += "<td>" + status + "</td>";
+
+				
+				output += "<td><input name='btnUpdate' type='button' value='Update' class='btnUpdate btn btn-secondary'></td>"
+						+ "<td><input name='btnRemove' type='button' value='Remove' class='btnRemove btn btn-danger' data-userid='"
+						+ id + "'>" + "</td></tr>";
+			}
+
+			con.close();
+
+			// Complete the html table
+			output += "</table>";
+		} catch (Exception e) {
+			output = "Error while reading the projects.";
+			System.err.println(e.getMessage());
+		}
+
+		return output;
+	}
+
+	public String updateProject(String id, String name, String email, String password,String AccountNo) {
+		String output = "";
+
+		try {
+			Connection con = connect();
+
+			if (con == null) {
+				return "Error while connecting to the database for updating.";
+			}
+
+			// create a prepared statement
+			String query = "UPDATE user SET name=?,email=?,password=?,AccountNo=?,otp=? WHERE id=?";
+
+			PreparedStatement preparedStmt = con.prepareStatement(query);
+
+			// binding values
+
+			preparedStmt.setString(1, name);
+			preparedStmt.setString(2, email);
+			preparedStmt.setString(3, password);
+			preparedStmt.setString(4, AccountNo);
+			preparedStmt.setInt(5, Integer.parseInt(id));
+
+			// execute the statement
+			preparedStmt.execute();
+			con.close();
+
+			String newProject = readProject();
+			output = "{\"status\":\"success\", \"data\": \"" + newProject + "\"}";
+		} catch (Exception e) {
+			output = "{\"status\":\"error\", \"data\": \"Error while updating the project.\"}";
+			System.err.println(e.getMessage());
+		}
+
+		return output;
+	}
+
+	public String deleteProject(String id) {
+
+		String output = "";
+
+		try {
+			Connection con = connect();
+
+			if (con == null) {
+				return "Error while connecting to the database for deleting.";
+			}
+
+			// create a prepared statement
+			String query = "delete from user where id=?";
+
+			PreparedStatement preparedStmt = con.prepareStatement(query);
+
+			// binding values
+			preparedStmt.setInt(1, Integer.parseInt(id));
+
+			// execute the statement
+			preparedStmt.execute();
+			con.close();
+
+			String newProject = readProject();
+			output = "{\"status\":\"success\", \"data\": \"" + newProject + "\"}";
+		} catch (Exception e) {
+			output = "Error while deleting the project.";
+			System.err.println(e.getMessage());
+		}
+
+		return output;
+	}
+
+}
